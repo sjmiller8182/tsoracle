67bb1dcbd8b92f034e113ce338c164bbc68efaaf
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-02-13 20:11:24 -0600

    Correct name in setup file

--- tsoracle/setup.py
+++ tsoracle/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="forepy",
+    name="tsoracle",
     version="0.0.dev0",
     author="Stuart Miller",
     author_email="s-miller@ti.com",
     description="Simple time series analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/sjmiller8182/tsoracle",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
