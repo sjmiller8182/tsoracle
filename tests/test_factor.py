6b33e1a8d2ee4083257bd61450ab0510f2c65278
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-02-21 16:50:13 -0600

    Add support for factor table

--- tests/test_factor.py
+++ tests/test_factor.py
@@ -9,6 +9,30 @@ from numpy import ndarray
 
 class TestMultiply(unittest.TestCase):
 
+    def test_Root(self):
+        """Unit tests for Root
+        """
+        # verify a polynomial that has a real root
+        rt = factor.Root(-0.8, 0)
+        self.assertAlmostEqual(0.5, rt.frequency, 6)
+        self.assertAlmostEqual(1.25, rt.abs_recip, 6)
+        self.assertEqual('-0.80000', rt.as_string)
+        self.assertEqual('1.0+0.8000B', rt.polynomial)
+        
+        # verify a polynomial that has a real root
+        rt = factor.Root(0.8, 0)
+        self.assertAlmostEqual(0.0, rt.frequency, 6)
+        self.assertAlmostEqual(1.25, rt.abs_recip, 6)
+        self.assertEqual('0.80000', rt.as_string)
+        self.assertEqual('1.0-0.8000B', rt.polynomial)
+
+        # verify complex roots
+        rt = factor.Root(1.2, -0.4)
+        self.assertAlmostEqual(0.05120819117478338, rt.frequency, 6)
+        self.assertAlmostEqual(0.7906, rt.abs_recip, 6)
+        self.assertEqual('1.20000+-0.40000i', rt.as_string)
+        self.assertEqual('1.0-2.4000B+1.6000B^2', rt.polynomial)
+
     def test_get_roots(self):
         """Unit tests for get_roots
         """
--- tsoracle/factor.py
+++ tsoracle/factor.py
@@ -14,6 +14,170 @@ RECIPROCAL_2PI = 0.15915494309189535
 """Constant for system frequency calculation: 1 / (2 * pi)
 """
 
+def get_system_freq(first_order_coef: float, sec_order_coef: float) -> float:
+    """Calculates the system frequency for a 2nd degree polynomial
+    with complex roots.
+
+    Parameters
+    ----------
+    first_order_coef: float
+        The coefficient of the first order term.
+    sec_order_coef: float
+        The coefficient of the second order term.
+
+    Returns
+    -------
+    frequency: float
+        The system frequency (Hz)
+
+    """
+    # calculate the radial angle from coefficients
+    radial_angle = first_order_coef \
+                    / (2 * np.sqrt(np.negative(sec_order_coef)))
+
+    return (RECIPROCAL_2PI * np.arccos(radial_angle))
+
+class Root:
+    """Container for a polynomial root.
+    
+    Attributes
+    ----------
+    polynomial: scalar or list-like
+        The string representation of the 
+        polynomial
+    frequency: float
+        The sustem frequency associated with
+        the root.
+    as_string: str
+        The string representation of the root.
+    abs_recip: float
+        The absolute reciprocal of the complex
+        number.
+    """
+    def __init__(self, real_part, imag_part):
+        self.real_part = real_part
+        if imag_part < 0:
+            self.imag_part = -imag_part
+        else:
+            self.imag_part = imag_part
+        # the polynomial is stored in standard order
+        if self.imag_part != 0.0:
+            self.poly_coef = polyfromroots(
+                [
+                    np.complex(self.real_part,
+                               self.imag_part),
+                    np.complex(self.real_part,
+                               -self.imag_part),
+                ]).real
+        else:
+            self.poly_coef = polyfromroots(
+                [self.real_part]).real
+    
+    @property
+    def polynomial(self) -> str:
+        """Get the polynomial associated with
+        root as a string
+        """
+        char = str()
+        for i, element in enumerate(self.poly_coef[::-1]):
+            ele = np.round(element, 4)
+            if i == 0:
+                char += '{}'.format(ele)
+            elif i == 1:
+                if ele > 0.0:
+                    char += '+'
+                char += (
+                    '{0:.4f}'.format(ele)
+                    + 'B')
+            else:
+                if ele > 0.0:
+                    char += '+'
+                char += (
+                    '{0:.4f}'.format(ele) 
+                    + 'B^' 
+                    + str(i))
+        return char
+    
+    @property
+    def frequency(self) -> float:
+        """Calculate the frequeny of the system
+        """
+        if self.imag_part != 0.0:
+            phi_1 = np.negative(self.poly_coef[1])
+            phi_2 = np.negative(self.poly_coef[0])
+            freq = get_system_freq(phi_1, phi_2)
+        else:
+            if self.poly_coef[0] > 0:
+                freq = 0.5
+            else:
+                freq = 0.0
+        return freq
+        
+    
+    @property
+    def as_string(self) -> str:
+        """Return a string representation of the root
+        """
+        char = '{0:.5f}'.format(self.real_part)
+        if self.imag_part != 0.0:
+            char += '+-' + '{0:.5f}'.format(self.imag_part) + 'i'
+        return char
+
+    @property
+    def abs_recip(self) -> float:
+        """Get the absolute reciprocal of the
+        complex number
+        """
+        return (
+            np.round(
+                np.reciprocal(
+                    np.abs(
+                        np.complex(self.real_part,
+                                   self.imag_part)
+                    )
+                ), 4
+            )
+        )
+        
+def table(polynomial: ndarray) -> None:
+    """Create summary table of polynomial.
+    The table is printed
+    
+    Parameters
+    ----------
+    factors: like-like
+        List of factors to multiply together.
+        
+        Polynomial should be entered as phis and thetas.
+        
+        An example AR polynomial is
+        
+        (1 - 1.2 B + 0.4 B^2) X_t = a_t
+    
+        This would be entered as 
+        
+        multiply( [1.2, -0.4] )
+    
+    Acts like `tswge::factor.wge`
+    
+    """
+    
+    roots = np.roots( np.insert(np.negative(polynomial)[::-1], 0 ,1) )
+    # imag root pairs are treated as one
+    all_roots = roots[np.where(roots.imag >= 0.0)]
+    
+    # convert to table
+    print("Factor\t\t\tRoot(s)\t\t\tAbs Recip\tSystem Freq")
+    for root in all_roots:
+        conv_root = Root(root.real, root.imag)
+        print(
+            '{:24}'.format(conv_root.polynomial)
+            + '{:24}'.format(conv_root.as_string)
+            + '{:7.5f}'.format(conv_root.abs_recip)
+            + '{:16.5f}'.format(conv_root.frequency)
+        )
+        
+
 def get_roots(coef: Union[ndarray, List]) -> ndarray:
     """Calculate roots from an AR or MA polynomial
 
@@ -58,47 +222,6 @@ def roots_in_unit_circle(phi:Union[List, ndarray] = 0,
         roots_in_theta = False
 
     return roots_in_theta or roots_in_phi
-
-def get_system_freq(first_order_coef: float, sec_order_coef: float) -> float:
-    """Calculates the system frequency for a 2nd degree polynomial
-    with complex roots.
-
-    Parameters
-    ----------
-    first_order_coef: float
-        The coefficient of the first order term.
-    sec_order_coef: float
-        The coefficient of the second order term.
-
-    Returns
-    -------
-    frequency: float
-        The system frequency (Hz)
-
-    """
-    # calculate the radial angle from coefficients
-    radial_angle = first_order_coef \
-                    / (2 * np.sqrt(np.negative(sec_order_coef)))
-
-    return (RECIPROCAL_2PI * np.arccos(radial_angle))
-
-def table(polynomial: Union[ndarray, List]) -> str:
-    """Create a factor table from a polynomial
-
-    An example AR polynomial is
-        
-    (1 - 1.2 B + 0.4 B^2) X_t = 0
-
-    This would be entered as 
-    
-    table( [1.2, -0.4] )
-
-    Acts like `tswge::factor.wge`
-    """
-
-    roots = polyroots(polynomial)
-
-    raise NotImplementedError
 
 def to_glp(phi:Union[List, ndarray] = None,
            theta:Union[List, ndarray] = None,
